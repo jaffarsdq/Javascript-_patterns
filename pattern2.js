ee96b1315027da0db052273dfaa26731b7d383a2
Jaffar sadhiq <jaffarsdq007@gmail.com>
2022-11-24 21:49:54 +0530

    Initial commit

--- pattern2.js
+++ pattern2.js
@@ -8,8 +8,8 @@ function pattern(n) {
         for(let col = 1; col <= row; col += 1) { // only change is the limit of loop
             str += "*";
         }
-        console.log(str);
+        console.log(str)
     }
 }
 
-pattern(9);
+pattern(7);
